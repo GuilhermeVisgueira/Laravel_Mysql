68d12abad4ebce614f6ff5c276cd012427eced74
Guilherme DEV <guilhermervisgueira@gmail.com>
2026-03-19 13:35:16 -0300

    add file api.php

--- app/Models/Aluno.php
+++ app/Models/Aluno.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Http\Controllers\AlunoController;
 
 class Aluno extends Model
 {
---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EscolaController;
+
+//para cada controller se deve ter uma linha de comando relacionada ao ep que quero utilizar
+
