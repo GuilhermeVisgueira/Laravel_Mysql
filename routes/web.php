2a61fb66109fc0a412e15f7432f10ec0b1305fca
Guilherme DEV <guilhermervisgueira@gmail.com>
2026-04-07 15:56:27 -0300

    Add route(crud) escolas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\EscolaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+//Rotas Escola:
+//Get
+Route::get('/escolas', [EscolaController::class, 'showAll']);
+Route::get('/escolas/{id}', [EscolaController::class, 'searchForId']);
+Route::post('/escolas', [EscolaController::class, 'save']);
+Route::put('/escolas', [EscolaController::class, 'update']);
+Route::delete('/escolas', [EscolaController::class, 'delete']);
+
