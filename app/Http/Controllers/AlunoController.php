5ee9cb24274633e34df2fe27bb7af5e7b9542fcb
Guilherme DEV <guilhermervisgueira@gmail.com>
2026-04-09 14:06:41 -0300

    fix ControllerAluno

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -20,41 +20,39 @@ class AlunoController extends Controller
     {
         $alunoId = Aluno::find($id);
 
-        if(!$alunoId)
-            {
+        if (!$alunoId) {
 
-                return response()->json(["error" => "Id de aluno não encontrado"]);
+            return response()->json(["error" => "Id de aluno não encontrado"]);
 
-            }
-        return response()->json($alunoId,200);
+        }
+        return response()->json($alunoId, 200);
     }
 
     public function save(Request $request)
     {
-        $createAluno = Aluno::create($requeste->all());
-        return response()->json($createAluno. 201);
+        $createAluno = Aluno::create($request->all());
+        return response()->json($createAluno . 201);
     }
 
     public function update(Request $request, $id)
     {
         $aluno = Aluno::find($id);
-        if(!$aluno)
-            {
-                return response()->json(["Error" => "Aluno não encontrado"],404);
-            }
-        $aluno->update()->json($aluno,200);
-        return response()->json($aluno,200);
+        if (!$aluno) {
+            return response()->json(["Error" => "Aluno não encontrado"], 404);
+        }
+        $aluno->update()->json($aluno, 200);
+        return response()->json($aluno, 200);
     }
 
     public function delete($id)
     {
         $aluno = Aluno::find($id);
-        if(!$aluno)
-            {
-                return response()->json((["Erro"=>"Aluno não encontrado",404]));
+        if (!$aluno) {
+            return response()->json((["Erro" => "Aluno não encontrado", 404]));
+        }
+        $aluno->delete();
 
-            }
-    }   $aluno->delete();
-    return response()->json(["mensagem"=>"deletado com sucesso",200]);
+        return response()->json(["mensagem" => "deletado com sucesso", 200]);
 
+    }
 }
