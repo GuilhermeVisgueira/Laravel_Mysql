0adab0cdab84481e3423c1444e9c7dd40491ccc9
Guilherme DEV <guilhermervisgueira@gmail.com>
2026-04-13 11:30:21 -0300

    Add controller Escola_funcionario

--- app/Http/Controllers/EscolaFuncionarioController.php
+++ app/Http/Controllers/EscolaFuncionarioController.php
@@ -9,9 +9,47 @@ class EscolaFuncionarioController extends Controller
 {
     public function showAll()
     {
-        $listAll = EscolaFuncionarioController::all();
+        $listAll = EscolaFuncionario::all();
         return response()->json($listAll, 200);
     }
 
+    public function searchForId($id)
+    {
+        $EFuncionarioId = EscolaFuncionario::find($id);
+        
+        if(!$EFuncionarioId)
+            {
+               return response()->json(["Erro" => "Id de Escola_funcionario não encontrado",404]);
+            }
+        return response()->json($EFuncionarioId, 200);
+    }
+
+    public function save(Request $request)
+    {
+        $createEFuncionario = EscolaFuncionario::create($request->all());
+        return response()->json($createEFuncionario, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $EFuncionario = EscolaFuncionario::find($id);
+        if(!$EFuncionario)
+            {
+                return response()->json(["Erro" => "Escola_Funcionario não encontrado", 404]);
+            }
+        $EFuncionario->update($request->all());
+        return response()->json($EFuncionario, 200);
+    }
+
+    public function delete($id)
+    {
+        $EFuncionario = EscolaFuncionario::find($id);
+        if(!$EFuncionario)
+            {
+                return response()->json(["Erro" => "Escola_funcionario nao encontrado", 404]);
+            }
+        $EFuncionario->delete();
+        return response()->json(["Mensagem" => "Escola_funcionario deletado", 200]);
+    }
 
 }
