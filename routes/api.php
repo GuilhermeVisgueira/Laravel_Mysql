e42f77359e7da2e19e7aea84eb0906640c05ddda
Guilherme DEV <guilhermervisgueira@gmail.com>
2026-04-13 09:53:27 -0300

    Add /{id} in routes escola, turma, aluno

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ Route::get('/escolas', [EscolaController::class, 'showAll']);
 Route::get('/escolas/{id}', [EscolaController::class, 'searchForId']);
 Route::post('/escolas', [EscolaController::class, 'save']);
 Route::put('/escolas', [EscolaController::class, 'update']);
-Route::delete('/escolas', [EscolaController::class, 'delete']);
+Route::delete('/escolas/{id}', [EscolaController::class, 'delete']);
 
 //Rotas Escola_funcionario
 Route::get('/escola_funcionario', [FuncionarioController::class, '']);
@@ -26,12 +26,12 @@ Route::get("/turma", [TurmaController::class, "showAll"]);
 Route::get("/turma/{id}", [TurmaController::class, "searchForId"]);
 Route::post("/turma", [TurmaController::class, "save"]);
 Route::put("/turma", [TurmaController::class, "update"]);
-Route::delete("/turma", [TurmaController::class, "delete"]);
+Route::delete("/turma/{id}", [TurmaController::class, "delete"]);
 
 //Rotas Aluno:
 
-Route::get("/alunos", [AlunoController::class, "showAll"]);
+Route::get("/aluno", [AlunoController::class, "showAll"]);
 Route::get("/aluno/{id}", [AlunoController::class, "searchForId"]);
 Route::post("/aluno", [AlunoController::class, "save"]);
 Route::put("/aluno", [AlunoController::class, "update"]);
-Route::delete("/aluno", [AlunoController::class, "delete"]);
+Route::delete("/aluno/{id}", [AlunoController::class, "delete"]);
